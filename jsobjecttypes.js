aa1d33123ace51746803e98d570ae5d983b63a50
CodeSignDesign <codesigndesign@gmail.com>
2019-02-03 13:38:55 +0200

    JavaScript Object Type start

---
+++ jsobjecttypes.js
@@ -0,0 +1,13 @@
+// JavaScript Object Types
+// * * * * * * * * * * * * 
+
+/** 
+* Creating Project - {}, New
+* Accessing Properties - []
+* Accessign Object Properties with Variables
+* Updating Object Variables
+* Adding New Properties
+* Delete Properties
+* Object has Property?
+* Nested Object
+* /
