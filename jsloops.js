2b03191ee56eafc9260d24982cf9a18030c02f9b
CodeSignDesign <codesigndesign@gmail.com>
2019-02-06 15:04:51 +0200

    if else STATEMENT

--- jsloops.js
+++ jsloops.js
@@ -1,3 +1,21 @@
 // JavaScript LOOPS
+// JavaScript Statements
+// ====================
+/**
+ * if else
+ * do while
+ * while
+ * for
+ * for in
+ * labeled
+ * break continue
+ * switch
+ */
+// if STATEMENT is flow-control statement.
+// if STATEMENT EVALUATE A GIVEN CONDITION TO A Boolean true or false. 
+var num = 100;
+if (num < 50) {
+    alert('Yes it is grater than ' + num);
+} else {
 
-// if else statements
+}
