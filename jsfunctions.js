064dac28db2c69537ad2f2ca7daca0d83fa1b98a
CodeSignDesign <codesigndesign@gmail.com>
2019-02-06 19:43:07 +0200

    Finished Functions Basic

--- jsfunctions.js
+++ jsfunctions.js
@@ -1,2 +1,30 @@
 // JavaScript Functions
 // ===================
+/**
+ * With functions you can encapsulate statements
+ * that can be run everywhere
+ * functions can be called with name and arguments with comma
+ * not necessary to return a value
+ * functions stops executing on return statement
+ * you have have multiple returns
+ */
+// function total(one, two) {
+//     var totalThis = one + two;
+//     return totalThis;
+// }
+// var result = total(1, 2);
+// alert(result);
+// var anotherResult = total(100, 500);
+// alert(anotherResult);
+// total(); // to envoke function we have to use parentheses
+
+function nameCheck(name) {
+    if (name == 'CodeSign') {
+        return 'Hi CodeSign';
+    } else {
+        return 'I dont know you' 
+    }
+    alert(Otinanai);
+}
+var result = nameCheck('CodeSign');
+alert(result);
