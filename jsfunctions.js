3a9668d6dbe961ae4cffc56fe24b0e74888c3931
CodeSignDesign <codesigndesign@gmail.com>
2019-02-08 01:45:42 +0200

    JavaScript REFERENCE TYPE - Started

--- jsfunctions.js
+++ jsfunctions.js
@@ -47,11 +47,59 @@
 // }
 // alert(sayHi("JavaScript ", 'JS Functions are Great', 'Code '));
 
-function shopping() {
-    if (arguments.length >= 2) {
-        alert('You are granted 20% discount!');
-    } else if (arguments.length <= 1) {
-        alert('You are not getting any discount!')
-    }
-}
-shopping("Milk", "Bread");
+// function shopping() {
+//     if (arguments.length >= 2) {
+//         alert('You are granted 20% discount!');
+//     } else if (arguments.length <= 1) {
+//         alert('You are not getting any discount!')
+//     }
+// }
+// shopping("Milk", "Bread");
+
+// var color = "blue"; // GLOBAL CONTEXT = WINDOW OBJECT
+
+// function colorUpdate() {
+//     var anotherColor = "green";
+//     var color = "purple"; // return color; WILL NOT GO OUTSIDE AND LOOK FOR GLOBAL EXECUTION !!!
+//     return color; // This is IN GLOBAL CONTEXT = SCOPE CHAIN
+//     }
+// console.log(colorUpdate());
+
+// * SAMPLE WHAT WE CAN DO WITH THE CODE ABOVE:
+// var whatever = function colorUpdate() {
+// }
+// colorUpdate();
+
+/**
+ * NO BLOCK LEVEL SCOPE !!!
+ * ====================
+ * in other programming languages such as C or Java...
+ * code block enclosed by brackets have their own scope
+ * but it's not true in JavaScript - if | for
+ */
+// if(true) {
+//     var color = "red";
+// } 
+// console.log(color);
+
+// function saycolor () {
+//     var color = "green";
+// }
+// console.log(color); // HERE WE TRY TO ACCESS COLOR FROM GLOBAL SCOPE AND IT DOESN'T WORK !!!
+
+// HERE THE MOST IMMEDIATE IS THE FUNCTON'S CONTEXT
+// for ( var i = 0; i < 10; i++) {
+//     //
+// }
+// console.log(i);
+
+/**
+ * JavaScript REFERENCE TYPE
+ * ==============================
+ * IN TRADITIONAL PROGRAMMING LANGUAGES SUCH AS JAVA ...
+ * CLASSES ARE REFERENCE TYPE BUT IN 'JAVASCRIPT' IT'S OBJECT
+ * OBJECT TYPE IS BASE FROM WHICH ALL OTHER REFERENCE TYPES
+ * ... INHERIT BASIC BEHAVIOR. 
+ * all REFERENCE TYPES such as: Array, Date and RegExp ...
+ * ... INHERIT THEIR BASIC BEHAVIOR FROM OBJECT TYPE
+ */
