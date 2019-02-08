57b8b1fef6c7b4e085552c908fa02addf80a249b
CodeSignDesign <codesigndesign@gmail.com>
2019-02-09 00:33:38 +0200

    Finished create ARRAY

--- jsarrays.js
+++ jsarrays.js
@@ -17,5 +17,33 @@
  * reduce method - reduce(), reduceRight()
  */
 
- // CREATE ARRAY
- var colors = new Array();
+ // create ARRAY !!!
+//  var colors = new Array('red', 'green', 'yellow');
+//  console.log(colors);
+//  var colors = []; // HERE: array literal > PREFERRED WAY
+// var colors1 = ['purple', 'blue', 'green', 'white'];
+// console.log(colors1);
+// console.log(colors1.length);
+// with brackets we can access the particular array
+// console.log(colors1[2]);
+// console.log(colors1[0]);
+// console.log((colors1.length = 3));
+// console.log(colors.length = 2);
+// console.log((colors1.length = 100));
+// console.log(Array.isArray(colors1));
+
+// IT CAN HOLD ANY TYPE OF DATA: literal:['yellow', 'orange']
+// var colors = ['purple', 'blue', 'green', 'pink', 99, ['yellow', 'orange']];
+// alert(colors);
+// alert(colors[2]); // access using bracket notation
+// console.log(colors);
+// console.log(Array.isArray(colors));
+// SET VALUE USING BRACKET NOTATION:
+// alert(colors[2] = 'purple');
+// DETECTING ARRAYS USING Array.isArray()
+// console.log(Array.isArray(colors));
+// ANOTHER WAY THAT WE - CAN NOT - RELAY ON
+// console.log(colors instanceof Array);
+
+// NOW: conversion methods - toString(), join()
+// objects have: toString(), toLocalString() and valueOf()
