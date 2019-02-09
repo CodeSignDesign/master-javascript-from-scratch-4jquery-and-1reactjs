63c49c5ce13f32f782472b3455b5965fb32c57ba
CodeSignDesign <codesigndesign@gmail.com>
2019-02-09 04:06:42 +0200

    ADDED A BIT OF jQuery

---
+++ style.js
@@ -0,0 +1,8 @@
+// TO TARGET class IN jQuery PUT: $(".class");
+// TO TARGET id IN jQuery PUT: $("#id");
+// TARGETING IN SAME LIKE IN CSS
+$(document).ready(function(){
+
+    $(".btn").addClass("animated bounce");
+
+});
