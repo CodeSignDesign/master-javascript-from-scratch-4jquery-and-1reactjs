0f2c322b29f186901c5a41924ad6820f2e3f17cc
CodeSignDesign <codesigndesign@gmail.com>
2019-02-15 09:04:04 +0200

    51 STRING MANIPULATION METHODS - FINISHED

--- 11jsstrings-primitive-wrapper-type.js
+++ 11jsstrings-primitive-wrapper-type.js
@@ -40,7 +40,7 @@
 // console.log(num.toFixed(2)); // OUTPUT: 10.00
 
 /**
- * 51 STRING MANIPULATION METHODS
+ * ^ ^ ^ ^ 51 STRING MANIPULATION METHODS
  * concat(); - slice(); - substr(); - substring();
  */
 
@@ -54,18 +54,27 @@
 // * slice();
 // ^ slice(); METHOD SELECTS THE ELEMENTS STARTING AT THE GIVEN START ARGUMENT, AND ENDS AT, BUT DOES NOT INCLUDE, THE GIVEN END ARGUMENT ^ ^ ^ 
 // var strVal = 'hello world';
-// alert(strVal.slice(3)); // lo world
-// alert(strVal.slice(3, 7)); // lo w
+// alert(strVal.slice(3)); // "lo world" 
+// alert(strVal.slice(3, 7)); // "lo w"
 
 // * substring();
 // ^ substring(); EXTRACTS THE CHARACTERS IN A STRING BETWEEN "start" and "end", NOT INCLUDING "end" ITSELF
+// var strVal = 'hel*lo wor*ld';
 // var strVal = 'hello world';
-// alert(strVal.substring(3)); // lo world
-// alert(strVal.substring(3, 7)); // lo world
+// alert(strVal.substring(3)); // "lo world"
+// alert(strVal.substring(3, 7)); // "lo w" => MY RESULT @@@
+// SEND MESSAGE to Ryan BECAUSE OF THE DIFFERENCE and also say thanks and give an advice to start orthophonic classes
+// alert(strVal.substring(3, 7)); // "lo world"
+// sample from: https://www.w3schools.com/jsref/jsref_substring.asp
+// var str = "Hello world!"; // 0=H, 1=e, 2=l, 3=l, 4=o, 5=" ", 6="w", 7="o", 8="r", 9="l", 10="d" @@@###@@@
+// alert(str.substring(1, 4)); // "ell"
 
 // substr(); 
 // ^ substr(); EXTRACTS PART OF STRING, BEGINNING AT CHARACTER AT SPECIFIED POSITION, AND RETURNS THE SPECIFIC NUMBER OF CHARACTERS.
 // it gives the number of characters to return !!!
-var strVal = 'hello world';
-alert(strVal.substr(3)); // lo world
-alert(strVal.substr(3, 7)); // lo worl
+// var strVal = 'hello world';
+// alert(strVal.substr(3)); // "lo world"
+// alert(strVal.substr(3, 7)); // "lo worl"
+
+// 52 STRING MANIPULATION METHODS using:
+// indexOf(), trim(), toUpperCase(), toLowerCase()
