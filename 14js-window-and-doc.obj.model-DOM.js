813d677e1221de898f6b41af1c00eb26d835e07b
CodeSignDesign <codesigndesign@gmail.com>
2019-02-20 01:56:43 +0200

    64 BOM - Browser Object Model STARTED

--- 14js-window-and-doc.obj.model-DOM.js
+++ 14js-window-and-doc.obj.model-DOM.js
@@ -1,11 +1,14 @@
 /** WINDOW AND DUCUMENT OBJECT MODEL - DOM
  * 64 BOM - Browser Object Model
  * html DOCUMENT IS REPRESENTED AS ELEMENT nodes
- * WINDOW OBJECT PROPERTIES: document
+ * 
+ * WINDOW OBJECT PROPERTIES: 
+ * window.document
  * location - href / pathname
  * localstorage
  * navigator.geolocation
  * history - back/forward
+ * 
  * WINDOW OBJECT METHODS:
  * alert()
  * prompt()
