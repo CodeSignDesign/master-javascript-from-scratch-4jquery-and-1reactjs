70ecb47fc54da26eaf30b29a9b9afee7188d9ae8
CodeSignDesign <codesigndesign@gmail.com>
2019-02-21 09:53:19 +0200

    69 TARGETTING ALL ELEMENTS USING JWQUERY => STARTED

---
+++ 15jquery-basics.js
@@ -0,0 +1,18 @@
+// SECTION: 15 - JQUERY BASICS
+/** 68 jQuery Bootstrap Setup
+ * DOWNLOAD Starter template from Bootstrap website
+ */
+
+// alert('Hello Coder !');
+
+// START WRITING JQUERY - TEST: OK ! ! ! 
+// $(document).ready(function(){
+//     // EVERY CODE WE WRITE IS HERE: All jQuery CODE !!!
+//     alert('Hello Coder !');
+// });
+
+// 69 TARGETTING ALL ELEMENTS USING JWQUERY
+$(document).ready(function(){
+    // TARGET ALL THE "ELEMENTS" IN THE "DOM" WITH THE NAME OF "BUTTON"
+    $('button').addClass('');
+});
