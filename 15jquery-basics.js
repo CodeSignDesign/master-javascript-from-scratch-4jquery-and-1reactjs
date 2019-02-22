8ab1f5e85a0e1adbf7149e4cad5010d7670a6e0e
CodeSignDesign <codesigndesign@gmail.com>
2019-02-22 03:03:58 +0200

    72 REMOVE, APPEND AND CLONE in JQUERY  -> WORKING NOW !!!

--- 15jquery-basics.js
+++ 15jquery-basics.js
@@ -25,21 +25,25 @@
 // });
 
 // * * * 70 TARGETTING class AND id's IN jQuery
-/** ANIMATED CSS 
- * ATTENTION SEEKERS: bounce, flash, pulse, shake, swing, tada, wobble, jello, heartBeat
- * BOUNCING ENTRANCES: bounceIn, bounceInDown, ..Left, ..Right, ..Up
- * BOUNCING EXITS: bounceOut, bounceOutDown, - - | | -  -
- * FADING EXTRANCES - FADING EXITS: fadeInUpBig
- * FLIPPERS, LIGHTSPEED, ROTATING ENTRANCES, ROTATING EXITS
- * zoomIn, slideInUp
- */
+    /** ANIMATED CSS 
+     * ATTENTION SEEKERS: bounce, flash, pulse, shake, swing, tada, wobble, jello, heartBeat
+     * BOUNCING ENTRANCES: bounceIn, bounceInDown, ..Left, ..Right, ..Up
+     * BOUNCING EXITS: bounceOut, bounceOutDown, - - | | -  -
+     * FADING EXTRANCES - FADING EXITS: fadeInUpBig
+     * FLIPPERS, LIGHTSPEED, ROTATING ENTRANCES, ROTATING EXITS
+     * zoomIn, slideInUp
+     */
+
 $(document).ready(function(){
     $('button').addClass('animated bounce');
     $('.btn-success').addClass('shake');
     $('#target6').addClass('swing');
-    $('#target4').addClass('fadeInUp');
+    // $('#target4').addClass('fadeInUp');
     $('button').removeClass('btn-success');
 // * * * 71 CUSTOM CSS, TARGETTING PROPERTIES AND ADDING HTML IN jQUERY
+/** TARGETING THE ELEMENTS PROPERTIES:
+ * css, html, text, prop, addClass, removeClass, add html
+ */
     $('#target1').css('text-decoration', 'line-through');
     $('#target1').css('color', 'red');
     // FOR MULTIPLE OBJECTS USE: ! CURLY BRACES: ({name: 'value', nm: 'val'});
@@ -50,11 +54,12 @@ $(document).ready(function(){
     // $('h1').html('Learning jQuery is so much FUN !');
     $('h1').html('Learning <strong>jQuery</strong> is so much FUN !');
     $('h1').text('Learning jQuery is FUN !');
+// * * * 72 REMOVE, APPEND AND CLONE in JQUERY 
+    // REMOVE ELEMENT:
+    // $('#target1').remove();
+    // APPEND ELEMENT WITH: appendTo() COMMAND
+    $('#target4').appendTo('.left');
+    // clone() - appndTo() FUNCTIONS:
+    $('.left').clone().appendTo('.right');
+
 });
-/** TARGETING THE ELEMENTS PROPERTIES:
- * css, html, text, prop, addClass, removeClass, add html
- */
-
- // * * * 72 REMOVE, APPEND AND CLONE in JQUERY 
-
- 
