63dc2bea9c287fa7c1ef04720b7c636653eda726
CodeSignDesign <codesigndesign@gmail.com>
2019-02-22 08:35:18 +0200

    75 HANDLING EVENTS WITH JQUERY => STARTED

--- 15jquery-basics.js
+++ 15jquery-basics.js
@@ -1,6 +1,6 @@
 // SECTION: 15 - JQUERY BASICS
 
-// * * * 68 jQuery Bootstrap Setup
+// * * * 68 jQuery Bootstrap Setup * * * 
 //  DOWNLOAD Starter template from Bootstrap website
 
 // alert('Hello Coder !');
@@ -11,7 +11,7 @@
 //     alert('Hello Coder !');
 // });
 
-// * * * 69 TARGETTING ALL ELEMENTS USING JWQUERY
+// * * * 69 TARGETTING ALL ELEMENTS USING JWQUERY * * * 
 // - ADDED ANIMATE.CSS minified FROM: https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css
 // - NOT ADDED ANIMATE.CSS, this is just a reference: https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css
 
@@ -24,27 +24,26 @@
 //     $('button').addClass('animated bounce');
 // });
 
-// * * * 70 TARGETTING class AND id's IN jQuery
+// * * * 70 TARGETTING class AND id's IN jQuery * * * 
     /** ANIMATED CSS 
      * ATTENTION SEEKERS: bounce, flash, pulse, shake, swing, tada, wobble, jello, heartBeat
      * BOUNCING ENTRANCES: bounceIn, bounceInDown, ..Left, ..Right, ..Up
-     * BOUNCING EXITS: bounceOut, bounceOutDown, - - | | -  -
-     * FADING EXTRANCES - FADING EXITS: fadeInUpBig
+     * BOUNCING EXITS: bounceOut, bounceOutDown, #sameAsAbove       s 
+     * FADING EXTRANCES - FADING EXITS: fadeInUpBig 
      * FLIPPERS, LIGHTSPEED, ROTATING ENTRANCES, ROTATING EXITS
      * zoomIn, slideInUp
      */
 
 $(document).ready(function(){
-    $('button').addClass('animated bounce');
-    $('.btn-success').addClass('shake');
-    $('#target6').addClass('swing');
+    // $('button').addClass('animated bounce');
+    // $('.btn-success').addClass('shake');
+    // $('#target6').addClass('slideInRight');
     // $('#target4').addClass('fadeInUp');
     $('button').removeClass('btn-success');
-// * * * 71 CUSTOM CSS, TARGETTING PROPERTIES AND ADDING HTML IN jQUERY
-/** TARGETING THE ELEMENTS PROPERTIES:
- * css, html, text, prop, addClass, removeClass, add html
- */
-    $('#target1').css('text-decoration', 'line-through');
+// * * * 71 CUSTOM CSS, TARGETTING PROPERTIES AND ADDING HTML IN jQUERY * * * 
+// TARGETING THE ELEMENTS PROPERTIES:
+// css, html, text, prop, addClass, removeClass, add html
+    // $('#target1').css('text-decoration', 'line-through');
     $('#target1').css('color', 'red');
     // FOR MULTIPLE OBJECTS USE: ! CURLY BRACES: ({name: 'value', nm: 'val'});
     $('#target3').css({color: 'white', background: 'purple'});
@@ -54,12 +53,33 @@ $(document).ready(function(){
     // $('h1').html('Learning jQuery is so much FUN !');
     $('h1').html('Learning <strong>jQuery</strong> is so much FUN !');
     $('h1').text('Learning jQuery is FUN !');
-// * * * 72 REMOVE, APPEND AND CLONE in JQUERY 
-    // REMOVE ELEMENT:
-    // $('#target1').remove();
-    // APPEND ELEMENT WITH: appendTo() COMMAND
-    $('#target4').appendTo('.left');
-    // clone() - appndTo() FUNCTIONS:
-    $('.left').clone().appendTo('.right');
+// * * * 72 REMOVE, APPEND AND CLONE in JQUERY  * * * 
+// REMOVE ELEMENT:
+// $('#target1').remove();
+// APPEND ELEMENT WITH: appendTo() COMMAND
+    // $('#target4').appendTo('.left');
+// clone() - appndTo() FUNCTIONS: Chain 2 Methods SAMPLE: 
+    // $('.left').clone().appendTo('.right');
+
+    // * * * 73 TARGET PARENT, CHILD, EVEN AND ODD ITEMS IN JQUERY * * * 
+// Targeting: parent, children, specific children and even & odd numbers
+    // $('h2').parent().css('backgroundColor', 'yellow');
+    // $('.right').children().css('color', 'red');
+// TARGETING SPECIFIC CHILD: Sample 1::
+// $('.target:nth-child(2)').addClass('flash');
+// TARGETING SPECIFIC CHILD: Sample 2:    
+    // $('.target:nth-child(4)').addClass('animated swing');
+    // TARGETING odd numbers: VERY USEFUL WHEN WORKING WITH TABLES
+    // $('.target:odd').addClass('animated swing');
+// TARGETING even numbers: VERY USEFUL WHEN WORKING WITH TABLES
+    // $('.target:even').addClass('animated swing');
+
+    // * * * 74 JQUERY OF VANILLA JAVASCRIPT * * * 
+// IF YOU DON'T WANT TO WRITE JQUERY THERE IS SITE: ...
+// ...YOU MIGHT NOT NEED jQuery...
+// http://youmightnotneedjquery.com/
+
+    // * * * 75 HANDLING EVENTS WITH JQUERY * * * 
+// USING click EVENTS
 
 });
