128553d4d311463bacd30887c54795ed331a630e
CodeSignDesign <codesigndesign@gmail.com>
2019-02-27 04:02:12 +0200

    82 Adding - Preparing CSS Styling => FINISHED

--- 16jq-news-of-the-day-app.js
+++ 16jq-news-of-the-day-app.js
@@ -3,3 +3,8 @@
 // * * * 80 News of the Day App * * * 
 
 // * * * 81 Preparing HTML markup * * * 
+
+// * * * 82 Adding - Preparing CSS Styling * * * 
+
+// * * * 83 Making AJAX call to API and getting random item * * *
+
