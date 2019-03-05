bbb1bd27ded6dc6207fcb17e67a0c5e3efb7b6a8
CodeSignDesign <codesigndesign@gmail.com>
2019-03-05 08:54:18 +0200

    86 Go Live GitHub PAGE => DON'T WORK ONLINE

--- 16jq-news-of-the-day-app.js
+++ 16jq-news-of-the-day-app.js
@@ -19,5 +19,5 @@
 // * * * 85 Add clickable link to the news title * * * 
 
 // * * * 86 GO LIVE with GITHUB Pages * * * 
-// MY GitHub Page is:
+// MY GitHub Page is: DIN'T WORK ON LINE VERSION !!!
 // https://codesigndesign.github.io/master-javascript-from-scratch-4jquery-and-1reactjs/. 
